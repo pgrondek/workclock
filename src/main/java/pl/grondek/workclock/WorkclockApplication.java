c9094f6afb90dcf55796e0eb179f386aab63e7f9
pgrondek <pgrondek@gmail.com>
2018-04-30 23:39:54 +0200

    Fix Hibernate 5.2 configuration

--- src/main/java/pl/grondek/workclock/WorkclockApplication.java
+++ src/main/java/pl/grondek/workclock/WorkclockApplication.java
@@ -1,9 +1,12 @@
 package pl.grondek.workclock;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = HibernateJpaAutoConfiguration.class)
 public class WorkclockApplication {
 
     public static void main(String[] args) {
--- src/main/java/pl/grondek/workclock/configuration/HibernateConfiguration.java
+++ src/main/java/pl/grondek/workclock/configuration/HibernateConfiguration.java
@@ -1,58 +1,64 @@
 package pl.grondek.workclock.configuration;
 
 import org.apache.tomcat.dbcp.dbcp2.BasicDataSource;
-import org.hibernate.SessionFactory;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.autoconfigure.orm.jpa.JpaProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
-import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
-import java.io.IOException;
 import java.util.Properties;
 
 @Configuration
 @EnableTransactionManagement
+@EnableConfigurationProperties(JpaProperties.class)
 public class HibernateConfiguration {
 
-    @Bean
-    public SessionFactory sessionFactory() {
-        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
-        sessionFactory.setDataSource(dataSource());
-        sessionFactory.setPackagesToScan("pl.grondek.workclock.entity");
-        sessionFactory.setHibernateProperties(hibernateProperties());
-        try {
-            sessionFactory.afterPropertiesSet();
-        } catch (IOException ignored) {
-        }
-        return sessionFactory.getObject();
+    private final DataSourceProperties dataSourceProperties;
+    private final JpaProperties jpaProperties;
+    private final JpaProperties.Hibernate hibernateProperties;
+
+    public HibernateConfiguration(DataSourceProperties dataSourceProperties, JpaProperties jpaProperties) {
+        this.dataSourceProperties = dataSourceProperties;
+        this.jpaProperties = jpaProperties;
+        this.hibernateProperties = jpaProperties.getHibernate();
     }
 
     @Bean
     public DataSource dataSource() {
         BasicDataSource dataSource = new BasicDataSource();
-        dataSource.setDriverClassName("org.h2.Driver");
-        dataSource.setUrl("jdbc:h2:mem:db;DB_CLOSE_DELAY=-1");
-        dataSource.setUsername("sa");
-        dataSource.setPassword("sa");
+
+        dataSource.setDriverClassName(dataSourceProperties.getDriverClassName());
+        dataSource.setUrl(dataSourceProperties.getUrl());
+        dataSource.setUsername(dataSource.getUsername());
+        dataSource.setPassword(dataSource.getPassword());
 
         return dataSource;
     }
 
     @Bean
-    public PlatformTransactionManager hibernateTransactionManager() {
-        HibernateTransactionManager transactionManager = new HibernateTransactionManager();
-        transactionManager.setSessionFactory(sessionFactory());
-        return transactionManager;
+    public LocalSessionFactoryBean sessionFactoryBean() {
+        LocalSessionFactoryBean factoryBean = new LocalSessionFactoryBean();
+        Properties hibernateProperties = new Properties();
+
+
+        hibernateProperties.setProperty("hibernate.hbm2ddl.auto", this.hibernateProperties.getDdlAuto());
+
+        factoryBean.setDataSource(dataSource());
+        factoryBean.setHibernateProperties(hibernateProperties);
+        factoryBean.setPackagesToScan("pl.grondek.workclock.entity");
+
+        return factoryBean;
     }
 
-    private Properties hibernateProperties() {
-        Properties hibernateProperties = new Properties();
-        hibernateProperties.setProperty("hibernate.hbm2ddl.auto", "create-drop");
-        hibernateProperties.setProperty("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
-
-        return hibernateProperties;
+    @Bean
+    public HibernateTransactionManager transactionManager() {
+        HibernateTransactionManager transactionManager = new HibernateTransactionManager();
+        transactionManager.setSessionFactory(sessionFactoryBean().getObject());
+        return transactionManager;
     }
 }
